eda3b86068577ee8803af7d87e78a3db585c38d3
Jagga Bai <dzagur@post.pl>
2022-01-05 14:02:04 +0100

    Controllers + zastosowanie asynchronous tasks [w zamówieniach]

---
+++ Restauracja/Controllers/PozycjeZamowieniaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restauracja.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restauracja.Controllers
+{
+    public class PozycjeZamowieniaController : Controller
+    {
+        private readonly RestDbContext _context;
+        public PozycjeZamowieniaController(RestDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.PozycjeZamowienia.ToList();
+            return View();
+        }
+    }
+}
